912cefaf4483322864896f2eae2895c9712c6688
Eddy-123 <adegnandjoueddy12@gmail.com>
2023-01-28 20:57:17 +0100

    example of class based view with post list

--- book/blog/views.py
+++ book/blog/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from blog.models import Post
 
 
@@ -30,3 +32,9 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
